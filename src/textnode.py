2d991c8310f7a713c7d5249a75a7170d9cfc6c39
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-08-08 01:54:25 +0530

    Text Node

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self,TEXT, TEXT_TYPE, URL=None) -> None:
         self.text = TEXT
@@ -12,10 +21,18 @@ class TextNode:
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
-
-def main():
-    node = TextNode("This is a text node","bold","https://www.boot.dev")
-    print(node)
     
-if __name__ == "__main__":
-    main()
+def text_node_to_html_node(text_node: TextNode):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i",text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text,{"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img","", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
